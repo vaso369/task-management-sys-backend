c14b4835e1211726bda8c8e1b9cf1c412e95e4de
vaso369 <59292985+vaso369@users.noreply.github.com>
2020-02-21 14:49:28 +0100

    Update config.php

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,8 +1,8 @@
 <?php
 define("BASE_URL", $_SERVER['DOCUMENT_ROOT'].'/task-management-sys-v0/backend/app/');
-//define("LOG_FAJL", BASE_URL."/data/log.txt");
+define("LOG_FAJL", BASE_URL."/data/log.txt");
 define("ENV_FAJL", BASE_URL."/config/.env");
-//define("ERROR_FILE", BASE_URL."/data/errors.txt");
+define("ERROR_FILE", BASE_URL."/data/errors.txt");
 define("SERVER", "localhost");
 define("DATABASE", env("DATABASE"));
 define("USERNAME", env("USERNAME"));
